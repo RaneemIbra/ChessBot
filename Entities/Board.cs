15be7da1545004cf14a3498e1b0983bb8bf87d97
RaneemIbra <132738285+RaneemIbra@users.noreply.github.com>
2025-01-04 17:02:31 +0200

    Layed a small foundation for the board and the pieces, next we will need to make the user place the pieces across the board

---
+++ Entities/Board.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class Board
+{
+    private int[,] board;
+    public int NumOfBlackPieces { get; private set; }
+    public int NumOfWhitePieces { get; private set; }
+    public Board()
+    {
+        board = new int[8, 8];
+        NumOfBlackPieces = 0;
+        NumOfWhitePieces = 0;
+        InitBoard();
+    }
+
+    private void InitBoard()
+    {
+        for (int row = 0; row < 8; row++)
+        {
+            for (int col = 0; col < 8; col++)
+            {
+                board[row, col] = (int)Piece.Empty;
+            }
+        }
+    }
+}
